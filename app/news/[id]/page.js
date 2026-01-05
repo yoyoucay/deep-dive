1ed061d4b801ffaee5e85bbc5fbaf9642c58a983
yoyoucay <44655602+yoyoucay@users.noreply.github.com>
2026-01-05 19:44:54 +0700

    setup files, image, and create some pages

---
+++ app/news/[id]/page.js
@@ -0,0 +1,15 @@
+import { DUMMY_NEWS } from "@/dummy-news";
+export default function NewsPageDetails({params}) {
+    const newsSlug = params.id;
+    const newsItem = DUMMY_NEWS.find((newsItem) => newsItem.slug === newsSlug);
+    return (
+        <article className="news-article">
+        <header>
+            <img src={`/images/news/${newsItem.image}`} alt={newsItem.title} />
+            <h1>{newsItem.title}</h1>
+            <time dateTime={newsItem.date}>{newsItem.date}</time>
+        </header>
+        <p>{newsItem.content}</p>
+        </ article>
+    )
+}
