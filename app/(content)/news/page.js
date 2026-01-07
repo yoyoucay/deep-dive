e56f7eb637ec6eff7f9f3c1c7a792e1e298dea6e
yoyoucay <44655602+yoyoucay@users.noreply.github.com>
2026-01-07 20:04:45 +0700

    feat: refactor NewsPageto be server-side fetching data

--- app/(content)/news/page.js
+++ app/(content)/news/page.js
@@ -1,48 +1,18 @@
-'use client';
-
-import { useEffect, useState } from "react";
 import NewsList from "@/components/news-list";
 
-export default function NewsPage() {
-  const [isLoading, setIsLoading] = useState(false);
-  const [error, setError] = useState();
-  const [news, setNews] = useState();
+export default async  function NewsPage() {
+  const response = await fetch("http://localhost:8080/news");
 
-  useEffect(() => {
-    async function fetchNews() {
-      setIsLoading(true);
-      const response = await fetch("http://localhost:8080/news");
-      if (!response.ok) {
-        setError("Failed to fetch data");
-        setIsLoading(false);
-      }
-
-      const news = await response.json();
-      setIsLoading(false);
-      setNews(news);
-    }
-
-    fetchNews();
-  }, []);
-
-  if (isLoading) {
-    return <p>Loading...</p>;
-  }
-
-  if (error) {
-    return <p>{error}</p>;
-  }
-
-  let newsContent;
-
-  if (news){
-    newsContent = <NewsList news={news} />;
-  }
+  if (!response.ok) {
+    throw new Error("Failed to fetch news");
+  } 
+   
+  const news = await response.json();
 
   return (
     <>
       <h1>News Page</h1>
-      {newsContent}
+      <NewsList news={news} />
     </>
   );
 }
